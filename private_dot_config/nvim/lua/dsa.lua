173c1b1c7693b884392ab1990b1384225cce0333
Gamaliel Petralba <petralbagamaliel@gmail.com>
2025-03-24 01:14:48 +0800

    remove lua_old

--- private_dot_config/nvim/lua/dsa.lua
+++ private_dot_config/nvim/lua/dsa.lua
@@ -60,4 +60,8 @@ M.run_cpp_program = function()
         .nvim_buf_set_lines(output_buf, 0, -1, false, vim.split(output, "\n"))
 end
 
+-- Define user commands
+vim.api.nvim_create_user_command("OpenTestBuffer", dsa.open_test_buffer, {})
+vim.api.nvim_create_user_command("RunCpp", dsa.run_cpp_program, {})
+
 return M
