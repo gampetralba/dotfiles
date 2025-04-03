0968b186adcb41e87a7a264c1594059a9c96e32f
Gamaliel Petralba <petralbagamaliel@gmail.com>
2025-04-04 00:16:56 +0800

    set options first

--- private_dot_config/nvim/lua/config/lazy.lua
+++ private_dot_config/nvim/lua/config/lazy.lua
@@ -20,9 +20,9 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
+require("config.options")
 require("config.automcmds")
 require("config.keymaps")
-require("config.options")
 
 -- Setup lazy.nvim
 require("lazy").setup({
