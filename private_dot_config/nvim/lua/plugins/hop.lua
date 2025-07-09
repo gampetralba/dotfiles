c4411bee2947b72fa56cf222221b9321a5b92d47
Gamaliel Petralba <petralbagamaliel@gmail.com>
2025-07-09 23:29:31 +0800

    hop: keymaps work on visual mode

--- private_dot_config/nvim/lua/plugins/hop.lua
+++ private_dot_config/nvim/lua/plugins/hop.lua
@@ -2,7 +2,7 @@ return {
     'phaazon/hop.nvim',
     config = true,
     keys = {
-        {'F', "<cmd>lua require'hop'.hint_words()<cr>"},
-        {'f', "<cmd>lua require'hop'.hint_char1()<cr>"}
+        {'F', "<cmd>lua require'hop'.hint_words()<cr>", {mode = {"n", "v"}}},
+        {'f', "<cmd>lua require'hop'.hint_char1()<cr>", {mode = {"n", "v"}}}
     }
 }
