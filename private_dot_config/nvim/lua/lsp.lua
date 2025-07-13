3119370e7deedd1c30cb241ec1708f14c9eecf4e
Gamaliel Petralba <petralbagamaliel@gmail.com>
2025-07-13 20:35:12 +0800

    fix(lsp-modules): issues with ts_ls

--- private_dot_config/nvim/lua/lsp.lua
+++ private_dot_config/nvim/lua/lsp.lua
@@ -1,19 +1,19 @@
 local M = {}
 
-M.definition_new_tab = function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local method = "textDocument/definition"
-    local params = vim.lsp.util.make_position_params()
-    vim.cmd [[tabnew]]
-    vim.lsp.buf_request(bufnr, method, params)
+local function goto_definition(split_cmd)
+    local params = vim.lsp.util.make_position_params(0, 'utf-8')
+
+    vim.lsp.buf_request(0, "textDocument/definition", params,
+                        function(err, result)
+        if err or not result or vim.tbl_isempty(result) then return end
+
+        vim.cmd(split_cmd)
+        vim.lsp.util.show_document(result[1], "utf-8", {focus = true})
+    end)
 end
 
-M.definition_vsplit = function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local method = "textDocument/definition"
-    local params = vim.lsp.util.make_position_params()
-    vim.cmd [[vsplit]]
-    vim.lsp.buf_request(bufnr, method, params)
-end
+M.definition_new_tab = function() goto_definition("tabnew") end
+
+M.definition_vsplit = function() goto_definition("vsplit") end
 
 return M
