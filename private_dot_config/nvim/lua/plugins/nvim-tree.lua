95fe7221eb23b7bde8987e03cf21da69a016f87f
Gam <petralbagamaliel@gmail.com>
2026-08-31 02:10:32 +0800

    feat(nvim): preview markdown in browser from nvim-tree

--- private_dot_config/nvim/lua/plugins/nvim-tree.lua
+++ private_dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -11,7 +11,35 @@ return {
                     git = true
                 }
             }
-        }
+        },
+        on_attach = function(bufnr)
+            local api = require("nvim-tree.api")
+            api.config.mappings.default_on_attach(bufnr)
+            vim.keymap.set("n", "<leader>om", function()
+                local node = api.tree.get_node_under_cursor()
+                if not (node and node.absolute_path and
+                    node.absolute_path:match("%.md$")) then return end
+                local out = "/tmp/pandoc-" ..
+                                vim.fn.fnamemodify(node.absolute_path, ":t:r") ..
+                                ".html"
+                vim.system({
+                    "pandoc", "-s", "-c",
+                    "https://cdn.simplecss.org/simple.min.css",
+                    node.absolute_path, "-o", out
+                }, {}, function(obj)
+                    if obj.code == 0 then
+                        vim.schedule(function()
+                            vim.fn.jobstart({"open", out}, {detach = true})
+                        end)
+                    else
+                        vim.schedule(function()
+                            vim.notify("pandoc failed: " .. obj.stderr,
+                                       vim.log.levels.ERROR)
+                        end)
+                    end
+                end)
+            end, {buffer = bufnr, desc = "render markdown to browser"})
+        end
     },
     keys = {
         {"<C-n>", "<cmd>NvimTreeToggle<CR>", desc = "nvim-tree toggle"},
